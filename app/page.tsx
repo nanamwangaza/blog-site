66fec5d7f805b21440622e593d762c370a646a65
Nana Mwangaza <nanamwangaza60@gmail.com>
2025-05-31 22:37:31 +0300

    add eslintignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,4 @@
-import { prisma } from "./utils/db"
-
-async function getData(){
-const data = await prisma.blogPost.findMany({
-  select:{
-    title:true,
-    content:true,
-    authorName:true,
-  }
-});
-return data;
-}
-
 export default async function Home() {
-  const data =await getData();
   return (
     <div>
       <h1>Welcome home</h1>
